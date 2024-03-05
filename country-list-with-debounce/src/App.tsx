be86fac2fe63d6971afae599a41ece7170a53d41
Hevin <122536372+hevinkalathiya@users.noreply.github.com>
2024-03-05 15:28:32 +0530

    Add heading to the app

--- country-list-with-debounce/src/App.tsx
+++ country-list-with-debounce/src/App.tsx
@@ -4,6 +4,9 @@ import CountriesList from "./CountriesList";
 function App() {
   return (
     <>
+      <h1 className="bg-gradient-to-r from-blue-500 to-cyan-500 p-3 text-white text-center text-3xl font-bold">
+        Country List
+      </h1>
       <CountriesList />
     </>
   );
